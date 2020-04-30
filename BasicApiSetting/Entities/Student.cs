7fe6f8fdf8504f38b1c78bc4070b9d763736a3ff
Omar Faruk Munna <omarmunna67@outlook.com>
2020-04-30 13:52:28 +0600

    class added

---
+++ BasicApiSetting/Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicApiSetting.Entities
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+    }
+}
